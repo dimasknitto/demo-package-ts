995097d54eed1630e40db120e681d5454e06c8e3
Dimas Knitto <it08.knitto@gmail.com>
2023-12-26 08:28:03 +0700

    fix: types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+interface SumType {
+    number1: number,
+    number2: number
+}
 function functionSum({ number1, number2 }: SumType): Promise<number | string> {
     return new Promise((resolve, reject) => {
         if (number1 < 1 && number2 < 1) {
